2b067ea516d7ac917068370adfc3ccaca6792fa6
sazeduzzaman <szamansaju@gmail.com>
2025-06-17 00:10:47 +0600

    Stop tracking dist folder

--- dist/src/utils/StripHtml/StripHtml.jsx
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.stripHtml = stripHtml;
-// utils/stripHtml.ts
-function stripHtml(html) {
-    return html.replace(/<\/?[^>]+(>|$)/g, "");
-}
